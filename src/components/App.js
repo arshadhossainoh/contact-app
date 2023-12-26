7c2448243f4bc561f86712b617feb2476d7f5d22
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-12-25 21:55:25 -0500

    created contact manager

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  const [contacts, setcontacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setcontacts([...contacts, contact]);
+  };
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
+    </div>
+  );
+}
+
+export default App;
